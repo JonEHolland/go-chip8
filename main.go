ebac648cd8376bbd354672a72836dfd66f45e69a
Jonathan Holland <joneholland@gmail.com>
2020-01-19 22:39:25 -0800

    Added Key IO

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
 			case *sdl.KeyboardEvent:
-				updateKeys(state, &event)
+				updateKeys(state, event)
 				break
 			case *sdl.WindowEvent:
 				// TODO - Handle Window Resize
@@ -85,6 +85,35 @@ func drawScreen(state *State, window *sdl.Window, renderer *sdl.Renderer) {
 	renderer.Present()
 	window.UpdateSurface()
 }
-func updateKeys(state *State, event *sdl.Event) {
+func updateKeys(state *State, event sdl.Event) {
+	switch t := event.(type) {
+	case *sdl.KeyboardEvent:
+		var direction = uint8(0)
+		if t.Type == sdl.KEYDOWN {
+			direction = uint8(1)
+		} else {
+			direction = uint8(0)
+		}
+		
+		switch t.Keysym.Sym {
+			case sdl.K_1: state.keyStates[0x1] = direction; break
+			case sdl.K_2: state.keyStates[0x2] = direction; break
+			case sdl.K_3: state.keyStates[0x3] = direction; break
+			case sdl.K_4: state.keyStates[0xC] = direction; break
+			case sdl.K_q: state.keyStates[0x4] = direction; break
+			case sdl.K_w: state.keyStates[0x5] = direction; break
+			case sdl.K_e: state.keyStates[0x6] = direction; break
+			case sdl.K_r: state.keyStates[0xD] = direction; break
+			case sdl.K_a: state.keyStates[0x7] = direction; break
+			case sdl.K_s: state.keyStates[0x8] = direction; break
+			case sdl.K_d: state.keyStates[0x9] = direction; break
+			case sdl.K_f: state.keyStates[0xE] = direction; break
+			case sdl.K_z: state.keyStates[0xA] = direction; break
+			case sdl.K_x: state.keyStates[0x0] = direction; break
+			case sdl.K_c: state.keyStates[0xB] = direction; break
+			case sdl.K_v: state.keyStates[0xF] = direction; break
+		}
+	break
+	}
 
 }
